295b7d14c085b1b8d25233060845fc2e8cbf7f96
BlazHercog1 <blaz.hercog@gmail.com>
2025-05-22 13:36:44 +0200

    Update data_capture.py

--- OSNOVE-RACUNALNISKEGA-VIDA/data/data_capture.py
+++ OSNOVE-RACUNALNISKEGA-VIDA/data/data_capture.py
@@ -2,15 +2,21 @@ import cv2
 import numpy as np
 import os
 
-def capture_images(user_id, count=50):
+def capture_images(user_id, output_dir="data/raw", count=50):
     cap = cv2.VideoCapture(0)
     img_count = 0
+    classifier = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     while img_count < count:
         ret, frame = cap.read()
         if not ret:
             break
-
-        #TODO
+  
+        faces = classifier.detectMultiScale(frame, 1.3, 5)
+        for (x, y, w, h) in faces:
+            face = frame[y:y+h, x:x+w]
+            cv2.imwrite(f"{output_dir}/{user_id}/{img_count}.jpg", face)
+            img_count += 1
+            print(f"Captured {img_count}/{count}")
         cv2.imshow('Capturing Faces', frame)
         if cv2.waitKey(1) == ord('q'):
             break
