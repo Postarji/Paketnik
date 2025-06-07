4d6cdbe701ea1aa508af131e57487393d91a7ecc
IrisSkledar <iris.skledar@student.um.si>
2025-06-07 13:16:51 +0200

    Updated userRoutes for 2fa

--- RAZVOJ-APLIKACIJ-ZA-INTERNET/backend/routes/userRoutes.js
+++ RAZVOJ-APLIKACIJ-ZA-INTERNET/backend/routes/userRoutes.js
@@ -2,6 +2,15 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/userController.js');
 
+function needsLogin(req, res, next){
+    if(req.session && req.session.userId){
+        return next();
+    } else {
+        var err = new Error("You must be logged in to view this page.");
+        err.status = 401;
+        return next(err);
+    }
+}
 
 router.get('/', userController.list);
 //router.get('/register', userController.showRegister);
@@ -14,6 +23,10 @@ router.post('/', userController.create);
 router.post('/login', userController.login);
 router.post('/login/face', userController.loginWithFace);
 router.post('/face-data', userController.updateFaceData);
+// NOVA POT za zahtevo nastavitve 2FA preko telefona
+router.post('/request-phone-2fa-setup', needsLogin, userController.requestPhone2FASetup);
+// NOVA POT za prijavo z obrazom preko spletne kamere
+router.post('/login-face-webcam', userController.loginFaceWebcam);
 
 router.put('/:id', userController.update);
 
