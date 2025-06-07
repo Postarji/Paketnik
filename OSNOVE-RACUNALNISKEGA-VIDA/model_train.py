2d88fa193f4ea21ff379d5d86ec31aaf160c8a70
IrisSkledar <iris.skledar@student.um.si>
2025-06-07 18:42:11 +0200

    fixed

--- OSNOVE-RACUNALNISKEGA-VIDA/model_train.py
+++ OSNOVE-RACUNALNISKEGA-VIDA/model_train.py
@@ -11,6 +11,7 @@ from keras._tf_keras.keras.utils import Sequence
 from keras import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 import matplotlib.pyplot as plt
+from config import AUGMENTED_DIR
 
 
 
@@ -57,7 +58,7 @@ class FaceSequence(Sequence):
 # === Nalaganje poti in oznak iz labels.csv ===
 def nalozi_poti_in_oznake():
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    base_path = os.path.join(script_dir, '..', 'data', 'augmented')
+    base_path = AUGMENTED_DIR 
     pot_csv = os.path.join(base_path, 'labels.csv')
     df = pd.read_csv(pot_csv)
     paths = [os.path.join(base_path, fname) for fname in df['filename']]
@@ -68,7 +69,7 @@ def nalozi_poti_in_oznake():
 
 
 # === CNN model za klasifikacijo obrazov ===
-def zgradi_model(capacity=3, vhodna_oblika=(224, 224, 3), razredi=1):
+def zgradi_model(capacity=3, vhodna_oblika=(224, 224, 3), razredi=1, koncna_aktivacija='sigmoid'):
     model = Sequential()
     kanali = 32
     for i in range(capacity):
@@ -82,6 +83,8 @@ def zgradi_model(capacity=3, vhodna_oblika=(224, 224, 3), razredi=1):
     model.add(Dense(64, activation='sigmoid'))
     model.add(Dense(64, activation='sigmoid'))
     model.add(Dense(1, activation='sigmoid'))  # Ena sama enota za binarno klasifikacijo
+    model.add(Dense(razredi, activation=koncna_aktivacija)) # Uporabi posredovano aktivacijo
+
     return model
 
 
@@ -131,7 +134,7 @@ def main():
 
         model.save(f"model_obrazi_kapaciteta_{capacity}.keras")
         narisi_grafe(history, f"kapaciteta_{capacity}")
-        
+
     if label_map:
         # Določi pot relativno glede na lokacijo TE datoteke (model_train.py)
         script_dir = os.path.dirname(os.path.abspath(__file__))
